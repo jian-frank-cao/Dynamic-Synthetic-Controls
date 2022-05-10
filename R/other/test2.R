4715cd22ee3c2124afac69ba0612af8cfff43150
Jian Cao <jccit@caltech.edu>
2022-05-10 13:17:39 +0100

    update

--- R/other/test2.R
+++ R/other/test2.R
@@ -126,7 +126,7 @@ ggplot(df, aes(time,value)) + geom_line(aes(colour = unit))
 
 ## grid search -----------------------------------------------------------------
 res_grid = NULL
-for (width in (1:6)*2+3) {
+for (width in (2:6)*2+3) {
   for (k in 4:7) {
     for (dtw1_time in 1985:1992) {
       ## Data ------------------------------------------------------------------------
@@ -220,7 +220,110 @@ for (width in (1:6)*2+3) {
     }
   }
 }
-                
+          
+res_grid = NULL      
+for (width in (1:6)*2+3) {
+  for (k in 4:7) {
+    for (dtw1_time in 1985:1992) {
+      temp = data.frame(width = width,
+                        k = k,
+                        dtw1_time = dtw1_time,
+                        ratio = NA_real_)
+      res_grid = rbind(res_grid, temp)
+    }
+  }
+}
 
+res_grid = left_join(res_grid[,-4], res_grid2, by = c("width", "k", "dtw1_time"))
 
-
+for (i in which(is.na(res_grid$ratio))) {
+  width = res_grid$width[i]
+  k = res_grid$k[i]
+  dtw1_time = res_grid$dtw1_time[i]
+  
+  ## Data ------------------------------------------------------------------------
+  load("./data/smoking.rda")
+  prop99 = read.csv("./data/prop99.csv")
+  
+  exclude_states = c("Massachusetts", "Arizona", "Oregon", "Florida",
+                     "Alaska", "Hawaii", "Maryland", "Michigan",
+                     "New Jersey", "New York",
+                     "Washington", "District of Columbia")
+  states = data.frame(id = 1:39,
+                      state = sort(setdiff(unique(prop99$LocationDesc),
+                                           exclude_states)))
+  smoking = smoking %>% mutate_all(as.numeric)
+  colnames(smoking)[1] = "id"
+  smoking = right_join(states, smoking, by = "id")
+  colnames(smoking)[2:4] = c("unit", "time", "value")
+  smoking = smoking %>% mutate(value_raw = value)
+  
+  ## Pre-processing --------------------------------------------------------------
+  values = reshape2::dcast(smoking %>% select(c("unit", "time", "value_raw")),
+                           time ~ unit, value.var = "value_raw")
+  
+  # transform
+  values = values %>% mutate_at(setdiff(colnames(values), "time"),
+                                ~normalize(., "t"))
+  
+  # adding buffer
+  add_buffer = function(TS, n){
+    model_right = forecast::auto.arima(TS)
+    right <- as.numeric(forecast::forecast(model_right, h = n)$mean)
+    model_left = forecast::auto.arima(rev(TS))
+    left <- rev(as.numeric(forecast::forecast(model_left, h = n)$mean))
+    
+    return(c(left, TS, right))
+  }
+  
+  values2 = sapply(values %>% select(-time),
+                   add_buffer, n = (width - 1)/2) %>% 
+    data.frame(.)
+  
+  # derivative
+  values2 = values2 %>%
+    mutate_all(~signal::sgolayfilt(., 3, width, 2)) %>%
+    .[((width - 1)/2 + 1):((width - 1)/2 + nrow(values)),]
+  values[-1] = values2
+  
+  # join data 
+  df <- reshape2::melt(values ,  id.vars = 'time',
+                       variable.name = 'unit')
+  
+  smoking = right_join(df, smoking %>% select(-value), by = c("time", "unit"))
+  smoking$age15to24 = smoking$age15to24*100
+  smoking = smoking[c("id", "unit", "time", "value", 
+                      "lnincome", "beer", "age15to24",
+                      "retprice", "value_raw")]
+  
+  ## run
+  print(paste0(paste0(c(width, k, dtw1_time), collapse = "-"), "...start..."))
+  units = smoking[c("id", "unit")] %>% distinct
+  result = as.list(1:39) %>% 
+    future_map(
+      ~{
+        i = .
+        dependent = units$unit[i]
+        dependent_id = units$id[i]
+        print(paste0(dependent, ":", i, "-", k, " start..."))
+        res = compare_methods(data = smoking,
+                              start_time = 1970,
+                              end_time = 1995,
+                              treat_time = 1985,
+                              dtw1_time = dtw1_time,
+                              dependent = dependent,
+                              dependent_id = dependent_id,
+                              normalize_method = "t",
+                              k = k,
+                              step.pattern = dtw::symmetricP2)
+        print(paste0(dependent, ":", i, "-", k, " start...Done."))
+        res$mse %>% mutate(dependent = dependent, k = k)
+      }
+    )
+  
+  result = result %>% 
+    do.call("rbind", .) %>% 
+    mutate(ratio = (mse1_post - mse2_post)/mse1_post)
+  
+  res_grid$ratio[i] = length(which(result$ratio>0))/39
+}
