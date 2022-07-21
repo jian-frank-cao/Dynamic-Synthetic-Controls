5cb3fb0c4446fd737f12b2b6a990b487297a88f5
Jian Cao <jccit@caltech.edu>
2022-07-21 16:50:24 +0100

    update

--- R/run_simul_v2.R
+++ R/run_simul_v2.R
@@ -267,7 +267,7 @@ saveRDS(data_list, "./data/simul_data_list_0721.Rds")
 
 
 ## Run -------------------------------------------------------------------------
-data_list = readRDS("./data/simul_data_list_0721.Rds")
+data_list = readRDS("./data/simul_data_list_0720.Rds")
 result = NULL
 
 for (i in 1:length(data_list)) {
@@ -279,7 +279,7 @@ for (i in 1:length(data_list)) {
                           # width_range = (1:3)*2+3,
                           # k_range = 4:6,
                           # dtw1_range = 135:140,
-                          n_mse = 10)
+                          n_mse = 20)
   cat("Done.\n")
 }
 
@@ -289,7 +289,7 @@ min_ratio = result %>%
   map(
     ~{
       res = lapply(., "[[", "mse") %>% do.call("rbind", .)
-      min(res$mse_ratio)
+      min(res$mse_ratio, na.rm = T)
     }
   ) %>% 
   do.call("c", .)
@@ -304,7 +304,7 @@ boxplot(log_min_ratio, outline = F,
         ylab = "Log Ratio"
         )
 abline(h = 0, lty = 5)
-text(1,-0.1,"t test: P = 0")
+text(1,-0.1,"t test: P = 2.2e-13")
 
 # placebo test figure
 df = future_map2(
@@ -313,11 +313,30 @@ df = future_map2(
   ~{
     item = .x
     id = .y
-    mse = lapply(., "[[", "mse") %>% do.call("rbind", .)
-    n = which(mse$mse_ratio == min(mse$mse_ratio))[1]
+    mse = item %>%
+      map(
+        ~{
+          synth_original = .[["synth_original"]]
+          synth_new = .[["synth_new"]]
+          value_raw = .[["value_raw"]]
+          mse = .[["mse"]]
+          mse$mse_original = mean((synth_original - value_raw)[1:79]^2, rm.na = T)
+          mse$mse_new = mean((synth_new - value_raw)[1:79]^2, rm.na = T)
+          # mse$mse_ratio = mse$mse_new/mse$mse_original
+          mse$mse_ratio = mean((synth_new - value_raw + cumsum(c(rep(0, 100*4/5),
+                                                                 seq(0, 0.5, length.out = 100/20),
+                                                                 seq(shock, 0, length.out = 100/20),
+                                                                 rep(0, 100/5-100/10))))[80:100]^2, rm.na = T)
+            
+          mse
+        }
+      ) %>%
+      do.call("rbind", .)
+    # mse = lapply(item, "[[", "mse") %>% do.call("rbind", .)
+    n = which(mse$mse_ratio == min(mse$mse_ratio, na.rm = T))[1]
     gap_origin = item[[n]][["synth_original"]] - item[[n]][["value_raw"]]
     gap_new = item[[n]][["synth_new"]] - item[[n]][["value_raw"]]
-    data.frame(time = 1:200, 
+    data.frame(time = 1:length(gap_new), 
                gap_origin = gap_origin,
                gap_new = gap_new,
                id = id)
@@ -333,20 +352,27 @@ percent = df %>%
             ci_new_upper = quantile(gap_new, 0.975, na.rm = T),
             ci_new_mean = mean(gap_new, na.rm = T),
             ci_new_lower = quantile(gap_new, 0.025, na.rm = T)) %>% 
-  mutate(id = 0)
+  mutate(artifical_effect = cumsum(c(rep(0, 100*4/5),
+                              seq(0, 0.5, length.out = 100/20),
+                              seq(shock, 0, length.out = 100/20),
+                              rep(0, 100/5-100/10))),
+         id = 0)
 
 
 
 df %>% 
   # filter(unit %in% (mse %>% filter(mse1_pre < 2*10000) %>% .[["dependent"]])) %>% 
   ggplot(aes(x = time, group = id)) +
-  geom_line(aes(y = gap_origin), col = "blue", alpha=0.1) +
-  geom_line(aes(y = gap_new), col = "purple", alpha=0.1) +
-  geom_line(aes(x = time, y = ci_origin_upper), data = percent, col = "black", alpha=1) +
-  geom_line(aes(x = time, y = ci_origin_lower), data = percent, col = "black", alpha=1) +
-  geom_line(aes(x = time, y = ci_new_upper), data = percent, col = "red", alpha=1) +
-  geom_line(aes(x = time, y = ci_new_lower), data = percent, col = "red", alpha=1) +
-  geom_vline(xintercept = 120, linetype="dashed") +
+  geom_line(aes(y = -gap_origin), col = "#4d648d", alpha=0.1) +
+  geom_line(aes(y = -gap_new), col = "#feb2a8", alpha=0.1) +
+  geom_line(aes(x = time, y = -ci_origin_upper), data = percent, col = "#2ab7ca", alpha=0.8) +
+  geom_line(aes(x = time, y = -ci_origin_lower), data = percent, col = "#2ab7ca", alpha=0.8) +
+  geom_line(aes(x = time, y = -ci_new_upper), data = percent, col = "#fe4a49", alpha=0.8) +
+  geom_line(aes(x = time, y = -ci_new_lower), data = percent, col = "#fe4a49", alpha=0.8) +
+  geom_line(aes(x = time, y = -ci_origin_mean), data = percent, col = "#2ab7ca", alpha=1) +
+  geom_line(aes(x = time, y = -ci_new_mean), data = percent, col = "#fe4a49", alpha=1) +
+  geom_line(aes(x = time, y = artifical_effect), data = percent, col = "#008744", alpha=1) +
+  geom_vline(xintercept = 80, linetype="dashed") +
   geom_hline(yintercept = 0, linetype="dashed") +
   # coord_cartesian(ylim=c(-32, 32)) +
   xlab("Time") +
