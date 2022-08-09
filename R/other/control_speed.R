060006e113116a1455990048835348767a7323ce
Jian Cao <jccit@caltech.edu>
2022-08-09 15:53:26 +0100

    update

--- R/other/control_speed.R
+++ R/other/control_speed.R
@@ -1,4 +1,4 @@
-set.seed(1)
+set.seed(10)
 
 # x - arima
 x = arima.sim(list(order = c(1,1,0), ar = c(0.8)),
@@ -15,17 +15,23 @@ plot(xDiff)
 
 # phi
 Diff2PhiUnif = function(xDiff,
-                    method = c("uniform"),
                     speed_upper = 1,
                     speed_lower = 0.5,
+                    reweight = TRUE,
                     rnd = 0.3
 ){
   pos_deriv = xDiff >= 0
   pos_ratio = sum(pos_deriv)/length(pos_deriv)
   speed = (rnd - 0.5)*2*(speed_upper - speed_lower) +
       sign(rnd - 0.5)*speed_lower
-  pos_speed = 1 + speed*(1 - pos_ratio)
-  neg_speed = 1 - speed*(pos_ratio)
+  if (reweight) {
+    pos_speed = 1 + speed*(1 - pos_ratio)
+    neg_speed = 1 - speed*(pos_ratio)
+  }else{
+    pos_speed = 1 + speed
+    neg_speed = 1 - speed
+    
+  }
   phi = rep(0, length(xDiff))
   phi[pos_deriv] = pos_speed
   phi[!pos_deriv] = neg_speed
