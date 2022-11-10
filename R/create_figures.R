ce48d3c7bcaf767d21d77967a05c0fdbcf9c24ee
Jian Cao <jccit@caltech.edu>
2022-11-10 16:11:23 +0000

    Update create_figures.R

--- R/create_figures.R
+++ R/create_figures.R
@@ -1,29 +1,54 @@
-## 1. Speed Difference ---------------------------------------------------------
-set.seed(4) # 3
+## Setup -----------------------------------------------------------------------
+library(checkpoint)
+checkpoint("2022-04-01")
 
+library(tidyverse)
+library(furrr)
+plan(multisession, workers = 8)
+options(future.rng.onMisuse="ignore")
+options(stringsAsFactors = FALSE)
+source("./R/misc.R")
+source("./R/TFDTW.R")
+source("./R/synth.R")
+source("./R/implement.R")
+source("./R/simulate.R")
+source("./R/grid.search.R")
+set.seed(20220407)
+
+
+## 1. Speed Difference ---------------------------------------------------------
 sim.data = function(n = 3, nCycles = 6, length = 100, extra.x = round(0.2*length),
                     t.treat = 60, shock = 0, ar.x = 0.6, n.SMA = 1,
                     n.diff = 1, speed.upper = 2, speed.lower = 0.5,
-                    reweight = TRUE, rescale = TRUE, 
+                    reweight = TRUE, rescale = TRUE,
                     rescale.multiplier = 20, beta = 1){
   # common exogenous shocks
-  x = sin(seq(0, nCycles * pi, length.out = length + extra.x + n.SMA + n.diff - 1))
+  x = cos(seq(0, nCycles * pi, length.out = length + extra.x + n.SMA + n.diff - 1))
+  x = x/2 + 0.5
   
+  xt = 0
+  x2 = NULL
+  for (j in 1:length(x)) {
+    xt = 0.96*xt + x[j]
+    x2 = c(x2, xt)
+  }
+  x = x2
+
   # smoothing
   x.SMA = ts(TTR::SMA(x, n = n.SMA)[-(1:(n.SMA - 1))])
-  
+
   # difference
   x.diff = diff(x.SMA, difference = n.diff)
   pos.diff = x.diff > 0
   if (reweight) {
     pos.ratio = sum(pos.diff)/sum(!pos.diff)
   }
-  
+
   # speeds
   log.speeds = seq(log(speed.lower), log(speed.upper), length.out = n)
   rnd.ind = 2
   log.speeds = c(log.speeds[rnd.ind], log.speeds[-rnd.ind])
-  
+
   # simulate
   data = NULL
   for (i in 1:n) {
@@ -41,18 +66,18 @@ sim.data = function(n = 3, nCycles = 6, length = 100, extra.x = round(0.2*length
       pos.speed = exp(log.speed)
       neg.speed = exp(-log.speed)
     }
-    
+
     phi.shape = rep(NA, length.out = length + extra.x)
     phi.shape[pos.diff] = pos.speed
     phi.shape[!pos.diff] = neg.speed
-    
+
     log.phi.mean = mean(log(phi.shape), na.rm = T)
     log.phi.sd = sd(log(phi.shape), na.rm = T)
-    
+
     phi.random = exp(rnorm(n = length + extra.x,
                            mean = log.phi.mean,
                            sd = log.phi.sd))
-    
+
     # treatment
     if (i == 1) {
       treatment = c(rep(0, t.treat),
@@ -61,21 +86,18 @@ sim.data = function(n = 3, nCycles = 6, length = 100, extra.x = round(0.2*length
     }else{
       treatment = 0
     }
-    
+
     phi = beta*phi.shape + (1 - beta)*phi.random
-    
+
     y = warpWITHweight(x[1:(length + extra.x)], phi)[1:length]
-    
-    y = y + rnorm(n = length(y), mean = 0, sd = 0.02)
-    
+
     if (rescale) {
       y = minmax.normalize(y, reference = y[1:t.treat])*rescale.multiplier
     }
-    
+
     y = y + treatment
-    
-    # y = cumsum(y)
-    
+    y = y + rnorm(n = length(y), mean = 0, sd = 0.2)
+
     data = rbind(data,
                  data.frame(id = i,
                             unit = LETTERS[i],
@@ -86,7 +108,7 @@ sim.data = function(n = 3, nCycles = 6, length = 100, extra.x = round(0.2*length
   return(data)
 }
 
-data = sim.data(n = 3, nCycles = 6, length = 100,
+data = sim.data(n = 3, nCycles = 8, length = 100,
                 t.treat = 60, shock = 0, ar.x = 0.6,
                 n.SMA = 1, n.diff = 1,
                 speed.upper = 2,
@@ -97,6 +119,76 @@ data = sim.data(n = 3, nCycles = 6, length = 100,
                 beta = 1)
 
 data %>% ggplot(aes(x = time, y = value, color = unit)) + geom_line()
+
+
+# parameters
+filter.width.range = (1:9)*2+3
+k.range = 4:9
+step.pattern.range = list(
+  # symmetricP0 = dtw::symmetricP0, # too bumpy
+  # symmetricP05 = dtw::symmetricP05,
+  symmetricP1 = dtw::symmetricP1,
+  symmetricP2 = dtw::symmetricP2,
+  # asymmetricP0 = dtw::asymmetricP0, # too bumpy
+  # asymmetricP05 = dtw::asymmetricP05,
+  asymmetricP1 = dtw::asymmetricP1,
+  asymmetricP2 = dtw::asymmetricP2,
+  typeIc = dtw::typeIc,
+  # typeIcs = dtw::typeIcs,
+  # typeIIc = dtw::typeIIc,  # jumps
+  # typeIIIc = dtw::typeIIIc, # jumps
+  # typeIVc = dtw::typeIVc,  # jumps
+  typeId = dtw::typeId,
+  # typeIds = dtw::typeIds,
+  # typeIId = dtw::typeIId, # jumps
+  mori2006 = dtw::mori2006
+)
+grid.search.parallel = TRUE
+
+
+args.TFDTW = list(buffer = 20, match.method = "open.end",
+                  dist.quant = 0.95, 
+                  window.type = "sakoechiba",
+                  ## other
+                  norm.method = "t",
+                  step.pattern2 = dtw::asymmetricP2,
+                  n.burn = 3, n.IQR = 3,
+                  ma = 3, ma.na = "original",
+                  default.margin = 3,
+                  n.q = 1, n.r = 1)
+
+args.synth = list(predictors = NULL,
+                  special.predictors = 
+                    expression(list(list(dep.var, 50:59, c("mean")),
+                                    list(dep.var, 40:49, c("mean")),
+                                    list(dep.var, 30:39, c("mean")))),
+                  time.predictors.prior = 1:59,
+                  time.optimize.ssr = 1:59)
+
+args.TFDTW.synth = list(start.time = 1, end.time = 100, treat.time = 60,
+                        args.TFDTW = args.TFDTW, args.synth = args.synth,
+                        ## 2nd
+                        n.mse = 10, 
+                        ## other
+                        plot.figures = FALSE,
+                        plot.path = "./figures/",
+                        legend.pos = c(0.3, 0.7))
+
+args.TFDTW.synth.all.units = list(target = "A",
+                                  # data = data, 
+                                  args.TFDTW.synth = args.TFDTW.synth,
+                                  ## 2nd
+                                  all.units.parallel = FALSE)
+
+args.TFDTW.synth.all.units[["data"]] = data
+results = SimDesign::quiet(
+  grid.search(filter.width.range = filter.width.range,
+              k.range = k.range,
+              step.pattern.range = step.pattern.range,
+              args.TFDTW.synth.all.units = args.TFDTW.synth.all.units,
+              grid.search.parallel = grid.search.parallel)
+)
+
 
 ## 3. Placebo Test -------------------------------------------------------------
 fig_all = ggpubr::ggarrange(fig_basque + ggpubr::rremove("ylab") +
