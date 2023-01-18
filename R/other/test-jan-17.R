ea491844d2514a7f84a10a95bc94c3ae647e92c9
Jian Cao <jccit@caltech.edu>
2023-01-18 17:44:13 +0000

    update

--- R/other/test-jan-17.R
+++ R/other/test-jan-17.R
@@ -4,8 +4,8 @@ checkpoint("2022-04-01")
 library(tidyverse)
 Wa = matrix(rep(0, 5*5), ncol = 5)
 
-Wb[1,1] = 1
-Wb[2,2:3] = 1
+Wa[1,1] = 1
+Wa[2,2:3] = 1
 Wa[3:4,3] = 1
 Wa[4,4:5] = 1
 Wa[5,5] = 1
@@ -36,5 +36,19 @@ weight.a.Rs = weight.a[2:4]
 weight.b = (W.pp.i %*% weight.a.Rs)/rowSums(as.matrix(W.pp.i))
 weight.b = as.numeric(weight.b)
 
+d = diag(x = 1, nrow = 5)
 res = Wa[,2:4] %*% W.pp.i
 warp2weight(res)
+
+B = diag(x = 1, nrow = 5)
+B[2:4, 2:4] = t(Wb)
+
+R = matrix(rep(1,5), ncol = 5)
+C = t(row.one)
+
+C3 = matrix(rep(1,3), nrow = 3)
+
+(Wa*(C%*%((R%*%Wa)^(-1))))%*%C
+a = (Wa%*%(diag(as.numeric((R%*%Wa)^(-1)))))%*%C
+b = ((diag(as.numeric((Wb%*%C3)^(-1))))%*%Wb)
+c = b %*% a[2:4,]
