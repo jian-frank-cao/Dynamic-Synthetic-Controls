3000987f0a6f75d81601d3a34c170526f0786535
Jian Cao <jccit@caltech.edu>
2023-03-30 22:21:30 +0100

    update

---
+++ R/backup/20230330/run_germany.R
@@ -0,0 +1,388 @@
+args = commandArgs(trailingOnly=TRUE)
+index = as.integer(args[1])
+job.start = Sys.time()
+
+## Setup -----------------------------------------------------------------------
+library(checkpoint)
+checkpoint("2022-04-01")
+
+library(tidyverse)
+library(furrr)
+plan(multisession, workers = 8)
+options(future.rng.onMisuse="ignore")
+options(stringsAsFactors = FALSE)
+
+source("./R/misc.R")
+source("./R/TFDTW.R")
+source("./R/synth.R")
+source("./R/implement.R")
+source("./R/grid.search.R")
+set.seed(20220407)
+
+
+## Germany Reunification Data --------------------------------------------------
+data = readRDS("./data/repgermany.Rds")
+colnames(data)[1:4] = c("id", "unit", "time", "value")
+data = data %>% mutate(value_raw = value)
+
+# rescale
+df.rescale = data %>% 
+  filter(time <= 1990) %>% 
+  group_by(unit) %>% 
+  summarise(value.min = min(value),
+            value.max = max(value)) %>% 
+  ungroup()
+
+mean.diff = mean(df.rescale$value.max - df.rescale$value.min)
+
+df.rescale = df.rescale %>% 
+  mutate(
+    multiplier = mean.diff/(value.max - value.min)
+  )
+
+data = left_join(data, df.rescale, by = "unit")
+data = data %>% 
+  mutate(
+    value.bak = value_raw,
+    value_raw = (value_raw - value.min)*multiplier,
+    value = value_raw
+  )
+
+# data list
+data.list = list(list(target = "West Germany",
+                      data = data))
+ids = data$id %>%
+  unique
+for (i in ids) {
+  data.temp = data %>% filter(!(id %in% c(i, 7)))
+  data.list = c(data.list, 
+                list(list(target = data.temp$unit[1],
+                          data = data.temp)))
+}
+
+select2 = combn(setdiff(ids, 7), 2, simplify = TRUE)[,1:100]
+
+for (i in 1:ncol(select2)) {
+  data.temp = data %>% filter(!(id %in% c(select2[, i], 7)))
+  data.list = c(data.list, 
+                list(list(target = data.temp$unit[1],
+                          data = data.temp)))
+}
+
+# ## Grid Search Germany ---------------------------------------------------------
+# parameters
+filter.width.range = (1:9)*2+3
+k.range = 4:9
+step.pattern.range = list(
+  # symmetricP0 = dtw::symmetricP0, # too bumpy
+  # symmetricP05 = dtw::symmetricP05,
+  symmetricP1 = dtw::symmetricP1,
+  symmetricP2 = dtw::symmetricP2,
+  # asymmetricP0 = dtw::asymmetricP0, # too bumpy
+  # asymmetricP05 = dtw::asymmetricP05,
+  asymmetricP1 = dtw::asymmetricP1,
+  asymmetricP2 = dtw::asymmetricP2,
+  typeIc = dtw::typeIc,
+  # typeIcs = dtw::typeIcs,
+  # typeIIc = dtw::typeIIc,  # jumps
+  # typeIIIc = dtw::typeIIIc, # jumps
+  # typeIVc = dtw::typeIVc,  # jumps
+  typeId = dtw::typeId,
+  # typeIds = dtw::typeIds,
+  # typeIId = dtw::typeIId, # jumps
+  mori2006 = dtw::mori2006
+)
+grid.search.parallel = TRUE
+
+
+args.TFDTW = list(buffer = 0, match.method = "fixed",
+                  dist.quant = 0.95,
+                  window.type = "none",
+                  ## other
+                  norm.method = "t",
+                  step.pattern2 = dtw::asymmetricP2,
+                  n.burn = 3, n.IQR = 3,
+                  ma = 3, ma.na = "original",
+                  default.margin = 3,
+                  n.q = 1, n.r = 1)
+
+args.synth = list(predictors = expression(c(dep.var,"trade","infrate")),
+                  special.predictors =
+                    expression(list(list("industry", 1981:1989, c("mean")),
+                                    list("schooling",c(1980,1985), c("mean")),
+                                    list("invest80" ,1980, c("mean")))),
+                  time.predictors.prior = 1981:1989,
+                  time.optimize.ssr = 1960:1989)
+
+args.TFDTW.synth = list(start.time = 1960, end.time = 2003, treat.time = 1990,
+                        args.TFDTW = args.TFDTW, args.synth = args.synth,
+                        ## 2nd
+                        n.mse = 10,
+                        ## other
+                        plot.figures = FALSE,
+                        plot.path = "./figures/",
+                        legend.pos = c(0.3, 0.8))
+
+args.TFDTW.synth.all.units = list(target = data.list[[index]]$target,
+                                  # data = data,
+                                  args.TFDTW.synth = args.TFDTW.synth,
+                                  ## 2nd
+                                  detailed.output = TRUE,
+                                  all.units.parallel = FALSE)
+
+args.TFDTW.synth.all.units[["data"]] = data.list[[index]]$data
+results = SimDesign::quiet(
+  grid.search(filter.width.range = filter.width.range,
+              k.range = k.range,
+              step.pattern.range = step.pattern.range,
+              args.TFDTW.synth.all.units = args.TFDTW.synth.all.units,
+              grid.search.parallel = grid.search.parallel)
+)
+
+saveRDS(results, paste0("./data/res_germany_1222_", index, ".Rds"))
+job.end = Sys.time()
+print(job.end - job.start)
+
+
+## Placebo ---------------------------------------------------------------------
+folder = "./data/placebo/germany/"
+file.list = as.list(list.files(folder))
+
+pre.start = 22
+pre.end = 31
+post.start = 32
+post.end = 41
+
+# de.mse
+df.mse = future_map2(
+  file.list,
+  as.list(1:length(file.list)),
+  ~{
+    file.name = .x
+    data.id = .y
+    data.list = readRDS(paste0(folder, file.name))
+    mse = future_map2(
+      data.list,
+      as.list(names(data.list)),
+      ~{
+        result.synth = .x[["results.TFDTW.synth"]]
+        grid.id = .y
+        mse = result.synth %>% 
+          map(
+            ~{
+              task = .
+              unit = task$dependent
+              gap.raw = task$gap.raw
+              gap.TFDTW = task$gap.TFDTW
+              data.frame(unit = unit,
+                         mse.preT.raw = mean(gap.raw[pre.start:pre.end]^2, na.rm = T),
+                         mse.preT.TFDTW = mean(gap.TFDTW[pre.start:pre.end]^2, na.rm = T),
+                         mse.postT.raw = mean(gap.raw[post.start:post.end]^2, na.rm = T),
+                         mse.postT.TFDTW = mean(gap.TFDTW[post.start:post.end]^2, na.rm = T))
+            }
+          ) %>% do.call("rbind", .)
+        mse %>% mutate(grid.id = grid.id)
+      }
+    ) %>% do.call("rbind", .)
+    mse$data.id = data.id
+    
+    mse %>% 
+      group_by(unit) %>% 
+      top_n(-1, mse.preT.TFDTW) %>% 
+      top_n(-1, grid.id)
+  }
+) %>% do.call("rbind", .)
+
+saveRDS(df.mse, "./data/df.mse_germany.Rds")
+
+# t.test for log(MSEdsc/MSEsc)
+df.mse = readRDS("./data/df.mse_germany.Rds")
+df.mse = df.mse %>% 
+  mutate(log.ratio = log(mse.postT.TFDTW/mse.postT.raw),
+         data.id = as.character(data.id))
+
+res.aov = aov(log.ratio ~ data.id*unit, df.mse)
+summary.aov = summary(res.aov)
+
+BMS = summary.aov[[1]]$`Mean Sq`[1]
+JMS = summary.aov[[1]]$`Mean Sq`[2]
+EMS = summary.aov[[1]]$`Mean Sq`[3]
+n = summary.aov[[1]]$`Df`[1] + 1
+k = summary.aov[[1]]$`Df`[2] + 1
+res.icc = (BMS - EMS)/(BMS + (k - 1)*EMS + k*(JMS - EMS)/n)
+res.vif = 1 + (k - 1)*res.icc
+DF = nrow(df.mse)/res.vif
+
+t.value = t.test(df.mse$log.ratio)$statistic
+p.value = pt(t.value, df = DF, lower.tail = TRUE)*2
+
+
+## Plot results ----------------------------------------------------------------
+# df.target
+results.target = readRDS("./data/res_germany_1204_1.Rds")
+target = "West Germany"
+
+pre.start = 22
+pre.end = 31
+post.start = 32
+post.end = 41
+
+mse = future_map2(
+  results.target,
+  as.list(names(results.target)),
+  ~{
+    result.synth = .x[["results.TFDTW.synth"]][[target]]
+    grid.id = .y
+    gap.raw = result.synth$gap.raw
+    gap.TFDTW = result.synth$gap.TFDTW
+    data.frame(grid.id = grid.id,
+               mse.preT.raw = mean(gap.raw[pre.start:pre.end]^2, na.rm = T),
+               mse.preT.TFDTW = mean(gap.TFDTW[pre.start:pre.end]^2, na.rm = T),
+               mse.postT.raw = mean(gap.raw[post.start:post.end]^2, na.rm = T),
+               mse.postT.TFDTW = mean(gap.TFDTW[post.start:post.end]^2, na.rm = T))
+  }
+) %>% do.call("rbind", .)
+
+opt.grid.id = mse %>% 
+  top_n(-1, mse.preT.TFDTW) %>% 
+  top_n(-1, grid.id) %>% 
+  .[["grid.id"]]
+
+df.target = data.frame(
+  time = 1960:2003,
+  unit = target,
+  data.id = 0,
+  grid.id = opt.grid.id,
+  value = results.target[[opt.grid.id]][[4]][[target]][[3]][["value"]],
+  synth.sc = results.target[[opt.grid.id]][[4]][[target]][[3]][["synthetic"]],
+  synth.dsc = results.target[[opt.grid.id]][[4]][[target]][[4]][["synthetic"]]
+)
+
+df.target = df.target %>% 
+  mutate(
+    gap.sc = value - synth.sc,
+    gap.dsc = value - synth.dsc,
+    group = paste0(data.id, "-", grid.id, "-", unit)
+  )
+
+saveRDS(df.target, "./data/df.target_germany.Rds")
+
+
+# df.gap
+df.mse = readRDS("./data/df.mse_germany.Rds")
+folder = "./data/placebo/germany/"
+file.list = as.list(list.files(folder))
+results = file.list %>%
+  future_map(
+    ~{
+      file.name = .
+      readRDS(paste0(folder, file.name))
+    }
+  )
+
+
+df.gap = NULL
+for (i in 1:nrow(df.mse)) {
+  unit = df.mse$unit[i]
+  data.id = df.mse$data.id[i]
+  grid.id = df.mse$grid.id[i]
+  df.gap[[i]] = data.frame(
+    time = 1960:2003,
+    unit = unit,
+    data.id = data.id,
+    grid.id = grid.id,
+    value = results[[data.id]][[grid.id]][[4]][[unit]][[3]][["value"]],
+    synth.sc = results[[data.id]][[grid.id]][[4]][[unit]][[3]][["synthetic"]],
+    synth.dsc = results[[data.id]][[grid.id]][[4]][[unit]][[4]][["synthetic"]]
+  )
+  print(i)
+}
+
+df.gap = df.gap %>%
+  do.call("rbind", .) %>%
+  mutate(
+    gap.sc = value - synth.sc,
+    gap.dsc = value - synth.dsc,
+    group = paste0(data.id, "-", grid.id, "-", unit)
+  )
+
+saveRDS(df.gap, "./data/df.gap_germany.Rds")
+
+# double check t test
+# res = df.gap %>%
+#   filter(time %in% 1991:2000) %>%
+#   group_by(unit, data.id, grid.id) %>%
+#   summarise(mse.sc = mean(gap.sc^2, na.rm = T),
+#             mse.dsc = mean(gap.dsc^2, na.rm = T)) %>%
+#   mutate(log.ratio = log(mse.dsc/mse.sc))
+# 
+# t.test(res$log.ratio)
+
+
+# plot
+df.target = readRDS("./data/df.target_germany.Rds")
+df.gap = readRDS("./data/df.gap_germany.Rds")
+
+df.quantile = df.gap %>%
+  group_by(time) %>%
+  summarise(quantile.sc.975 = quantile(gap.sc, 0.975, na.rm = T),
+            quantile.sc.025 = quantile(gap.sc, 0.025, na.rm = T),
+            quantile.dsc.975 = quantile(gap.dsc, 0.975, na.rm = T),
+            quantile.dsc.025 = quantile(gap.dsc, 0.025, na.rm = T)) %>% 
+  mutate(group = "quantile")
+
+color.sc = "#2ab7ca"
+color.dsc = "#fe4a49"
+# color.sc = "grey70"
+# color.dsc = "grey30"
+
+colors = c("Gap (SC)" = color.sc,
+           "Gap (DSC)" = color.dsc)
+
+fills = c("95% Quantile (SC)" = color.sc,
+          "95% Quantile (DSC)" = color.dsc)
+
+group.sample = sample(unique(df.gap$group), 100)
+
+fig.placebo = df.gap %>%
+  filter(group %in% group.sample) %>% 
+  ggplot(aes(x = time, group = group)) +
+  annotate("rect", xmin = 1990, xmax = 2000, 
+           ymin = -9000, ymax = 9000, alpha = .3) +
+  geom_line(aes(y = gap.sc), col = color.sc, alpha = 0.4) +
+  geom_line(aes(y = gap.dsc), col = color.dsc, alpha = 0.4) +
+  geom_ribbon(aes(ymin = quantile.sc.025,
+                  ymax = quantile.sc.975,
+                  fill="95% Quantile (SC)"),
+              data = df.quantile, alpha=0.5) +
+  geom_ribbon(aes(ymin = quantile.dsc.025,
+                  ymax = quantile.dsc.975,
+                  fill="95% Quantile (DSC)"),
+              data = df.quantile, alpha=0.5) +
+  geom_line(aes(y = gap.sc, color = "Gap (SC)"),
+            data = df.target, size = 1) +
+  geom_line(aes(y = gap.dsc, color = "Gap (DSC)"), 
+            data = df.target, size = 1) +
+  scale_color_manual(name = NULL, values = colors) +
+  scale_fill_manual(name = NULL, values = fills) +
+  geom_vline(xintercept = 1990, linetype="dashed", col = "grey20") +
+  geom_hline(yintercept = 0, linetype="dashed", col = "grey20") +
+  annotate("text", x = 1995, y = 6800,
+           label = "t = -4.0951\nP < 0.0001", col = "grey20") +
+  annotate("text", x = 1989, y = 4800, angle = 90,
+           label = "Treatment", col = "grey20") +
+  coord_cartesian(xlim=c(1970, 2010),ylim=c(-8000,8000)) +
+  xlab("Year") +
+  ylab("Gap(y - Synthetic Control)") +
+  theme_bw() +
+  theme(legend.position = c(0.25, 0.2),
+        legend.box = "horizontal",
+        legend.background = element_rect(fill=NA))
+
+# ggsave("./figures/placebo_germany.pdf",
+#        fig.placebo, width = 8, height = 6,
+#        units = "in", limitsize = FALSE)
+
+saveRDS(fig.placebo, "./data/placebo_germany.Rds")
+  
+
---
+++ R/backup/20230330/run_sim_shape.R
@@ -0,0 +1,331 @@
+args = commandArgs(trailingOnly=TRUE)
+i = as.integer(args[1])
+beta = as.integer(args[2])
+job.start = Sys.time()
+
+## Setup -----------------------------------------------------------------------
+library(checkpoint)
+checkpoint("2022-04-01")
+
+library(tidyverse)
+library(furrr)
+plan(multisession, workers = 8)
+options(future.rng.onMisuse="ignore")
+options(stringsAsFactors = FALSE)
+source("./R/misc.R")
+source("./R/TFDTW.R")
+source("./R/synth.R")
+source("./R/implement.R")
+source("./R/simulate.R")
+source("./R/grid.search.R")
+set.seed(20220407)
+
+
+## Data Simulation -------------------------------------------------------------
+# n.simulation = 150
+# length = 100
+# n = 10
+# 
+# # generate sobol sequence
+# # sobol.seq = qrng::sobol(n.simulation*1, d = n - 1, randomize = "Owen",
+# #                         seed = 20220401, skip = 100)
+# # rnd.speeds = cbind(rep(0.5, n.simulation), sobol.seq*0.5 + 0.1)
+# 
+# 
+# # simulate
+# data.list = NULL
+# for (i in 1:n.simulation) {
+#   data.list[[i]] = sim.data(n = n, length = length,
+#                             t.treat = 60, shock = 0, ar.x = 0.6,
+#                             n.SMA = 1, n.diff = 1,
+#                             speed.upper = 2,
+#                             speed.lower = 0.5,
+#                             reweight = TRUE,
+#                             rescale = TRUE,
+#                             rescale.multiplier = 20,
+#                             beta = 0.5)
+# }
+# 
+# data.list[[52]] %>% ggplot(aes(x = time, y = value, color = unit)) +
+#   geom_line() +
+#   geom_vline(xintercept = 60, linetype="dashed")
+# saveRDS(data.list, "./data/simul_data_beta_10.Rds")
+
+
+## Run -------------------------------------------------------------------------
+data.list = readRDS(paste0("./data/simul_data_beta_", beta, ".Rds"))
+
+# parameters
+filter.width.range = (1:9)*2+3
+k.range = 4:9
+step.pattern.range = list(
+  # symmetricP0 = dtw::symmetricP0, # too bumpy
+  # symmetricP05 = dtw::symmetricP05,
+  symmetricP1 = dtw::symmetricP1,
+  symmetricP2 = dtw::symmetricP2,
+  # asymmetricP0 = dtw::asymmetricP0, # too bumpy
+  # asymmetricP05 = dtw::asymmetricP05,
+  asymmetricP1 = dtw::asymmetricP1,
+  asymmetricP2 = dtw::asymmetricP2,
+  typeIc = dtw::typeIc,
+  # typeIcs = dtw::typeIcs,
+  # typeIIc = dtw::typeIIc,  # jumps
+  # typeIIIc = dtw::typeIIIc, # jumps
+  # typeIVc = dtw::typeIVc,  # jumps
+  typeId = dtw::typeId,
+  # typeIds = dtw::typeIds,
+  # typeIId = dtw::typeIId, # jumps
+  mori2006 = dtw::mori2006
+)
+grid.search.parallel = TRUE
+
+
+args.TFDTW = list(buffer = 20, match.method = "open.end",
+                  dist.quant = 0.95, 
+                  window.type = "sakoechiba",
+                  ## other
+                  norm.method = "t",
+                  step.pattern2 = dtw::asymmetricP2,
+                  n.burn = 3, n.IQR = 3,
+                  ma = 3, ma.na = "original",
+                  default.margin = 3,
+                  n.q = 1, n.r = 1)
+
+args.synth = list(predictors = NULL,
+                  special.predictors = 
+                    expression(list(list(dep.var, 50:59, c("mean")),
+                                    list(dep.var, 40:49, c("mean")),
+                                    list(dep.var, 30:39, c("mean")))),
+                  time.predictors.prior = 1:59,
+                  time.optimize.ssr = 1:59)
+
+args.TFDTW.synth = list(start.time = 1, end.time = 100, treat.time = 60,
+                        args.TFDTW = args.TFDTW, args.synth = args.synth,
+                        ## 2nd
+                        n.mse = 10, 
+                        ## other
+                        plot.figures = FALSE,
+                        plot.path = "./figures/",
+                        legend.pos = c(0.3, 0.7))
+
+args.TFDTW.synth.all.units = list(target = "A",
+                                  # data = data, 
+                                  args.TFDTW.synth = args.TFDTW.synth,
+                                  ## 2nd
+                                  all.units.parallel = FALSE)
+
+cat("Simulation data set ", i, "...")
+args.TFDTW.synth.all.units[["data"]] = data.list[[i]]
+results = SimDesign::quiet(
+  grid.search(filter.width.range = filter.width.range,
+              k.range = k.range,
+              step.pattern.range = step.pattern.range,
+              args.TFDTW.synth.all.units = args.TFDTW.synth.all.units,
+              grid.search.parallel = grid.search.parallel)
+)
+cat("Done.\n")
+
+saveRDS(results, paste0("./data/res_sim_beta_", beta, "_", i, ".Rds"))
+job.end = Sys.time()
+print(job.end - job.start)
+
+## Test result -----------------------------------------------------------------
+beta = 0
+shock = 10
+folder = "./data/res_sim/1011/"
+file.list = as.list(list.files(folder))
+
+length = 100
+treat_time = 60
+n_mse = 10
+treatment = c(rep(0, treat_time),
+              seq(0, shock, length.out = round(0.1*length)),
+              rep(shock, round(0.9*length - treat_time)))
+
+pre.start = 51
+pre.end = 60
+post.start = 61
+post.end = 70
+
+# de.mse
+df.mse = future_map2(
+  file.list,
+  as.list(1:length(file.list)),
+  ~{
+    file.name = .x
+    data.id = .y
+    data.list = readRDS(paste0(folder, file.name))
+    mse = future_map2(
+      data.list,
+      as.list(names(data.list)),
+      ~{
+        result = .x
+        grid.id = .y
+        value = result$res.synth.target.raw$value
+        synth.raw = result$res.synth.target.raw$synthetic
+        synth.TFDTW = result$res.synth.target.TFDTW$synthetic
+        gap.raw = value - synth.raw - treatment
+        gap.TFDTW = value - synth.TFDTW - treatment
+        data.frame(grid.id = grid.id,
+                   mse.preT.raw = mean(gap.raw[pre.start:pre.end]^2, na.rm = T),
+                   mse.preT.TFDTW = mean(gap.TFDTW[pre.start:pre.end]^2, na.rm = T),
+                   mse.postT.raw = mean(gap.raw[post.start:post.end]^2, na.rm = T),
+                   mse.postT.TFDTW = mean(gap.TFDTW[post.start:post.end]^2, na.rm = T))
+      }
+    ) %>% do.call("rbind", .)
+    mse$data.id = data.id
+    
+    mse %>% 
+      top_n(-1, mse.preT.TFDTW) %>% 
+      top_n(-1, grid.id)
+  }
+) %>% do.call("rbind", .)
+
+saveRDS(df.mse, paste0("./data/df.mse_sim_beta_", beta, ".Rds"))
+
+# t.test for log(MSEdsc/MSEsc)
+df.mse = readRDS(paste0("./data/df.mse_sim_beta_", beta, ".Rds"))
+df.mse = df.mse %>% 
+  mutate(log.ratio = log(mse.postT.TFDTW/mse.postT.raw))
+t.test(df.mse$log.ratio)
+
+
+## Plot result -----------------------------------------------------------------
+# df.gap
+df.mse = readRDS("./data/df.mse_sim_beta_1.Rds")
+folder = "./data/res_sim/1006/"
+file.list = as.list(list.files(folder))
+results = file.list %>%
+  future_map(
+    ~{
+      file.name = .
+      readRDS(paste0(folder, file.name))
+    }
+  )
+
+df.gap = NULL
+for (i in 1:nrow(df.mse)) {
+  data.id = df.mse$data.id[i]
+  grid.id = df.mse$grid.id[i]
+  df.gap[[i]] = data.frame(
+    time = 1:100,
+    data.id = data.id,
+    grid.id = grid.id,
+    value = results[[data.id]][[grid.id]][[5]][["value"]],
+    synth.sc = results[[data.id]][[grid.id]][[5]][["synthetic"]],
+    synth.dsc = results[[data.id]][[grid.id]][[6]][["synthetic"]]
+  )
+  print(i)
+}
+
+df.gap = df.gap %>%
+  do.call("rbind", .) %>%
+  mutate(
+    gap.sc = value - synth.sc,
+    gap.dsc = value - synth.dsc,
+    group = paste0(data.id, "-", grid.id)
+  )
+
+saveRDS(df.gap, "./data/df.gap_sim_beta_1.Rds")
+
+# plot
+df.gap = readRDS("./data/df.gap_sim_beta_1.Rds")
+
+# df.gap = df.gap %>% filter(time <= 80)
+
+shock = 10
+length = 100
+treat_time = 60
+n_mse = 10
+treatment = c(rep(0, treat_time),
+              seq(0, shock, length.out = round(0.1*length)),
+              rep(shock, round(0.9*length - treat_time)))
+
+df.quantile = df.gap %>%
+  group_by(time) %>%
+  summarise(mean.sc = mean(gap.sc, na.rm = T),
+            mean.dsc = mean(gap.dsc, na.rm = T),
+            quantile.sc.975 = quantile(gap.sc, 0.975, na.rm = T),
+            quantile.sc.025 = quantile(gap.sc, 0.025, na.rm = T),
+            quantile.dsc.975 = quantile(gap.dsc, 0.975, na.rm = T),
+            quantile.dsc.025 = quantile(gap.dsc, 0.025, na.rm = T)) %>% 
+  mutate(group = "quantile",
+         treatment = treatment)
+
+df.quantile[95:100, c("mean.dsc", "quantile.dsc.975", "quantile.dsc.025")] = NA
+
+
+color.sc = "#2ab7ca"
+color.dsc = "#fe4a49"
+# color.sc = "grey70"
+# color.dsc = "grey30"
+
+colors = c("Treatment Effect" = "grey20",
+           "Mean Gap (SC)" = color.sc,
+           "Mean Gap (DSC)" = color.dsc)
+
+fills = c("95% Quantile (SC)" = color.sc,
+          "95% Quantile (DSC)" = color.dsc)
+
+fig.big = df.gap %>%
+  ggplot(aes(x = time, group = group)) +
+  annotate("rect", xmin = 61, xmax = 70,
+           ymin = -25, ymax = 35, alpha = .3) +
+  geom_line(aes(y = gap.sc), col = color.sc, alpha=0.1) +
+  geom_line(aes(y = gap.dsc), col = color.dsc, alpha=0.1) +
+  geom_ribbon(aes(ymin = quantile.sc.025, ymax = quantile.sc.975,
+                  fill = "95% Quantile (SC)"), data = df.quantile, alpha=0.6) +
+  geom_ribbon(aes(ymin = quantile.dsc.025, ymax = quantile.dsc.975,
+                  fill = "95% Quantile (DSC)"), data = df.quantile, alpha=0.6) +
+  geom_line(aes(x = time, y = treatment, color = "Treatment Effect"),
+            data = df.quantile, alpha=1) +
+  geom_line(aes(x = time, y = mean.sc, color = "Mean Gap (SC)"),
+            data = df.quantile, alpha=1) +
+  geom_line(aes(x = time, y = mean.dsc, col = "Mean Gap (DSC)"),
+            data = df.quantile, alpha=1) +
+  scale_color_manual(name = NULL, values = colors) +
+  scale_fill_manual(name = NULL, values = fills) +
+  geom_vline(xintercept = 61, linetype="dashed", col = "grey20") +
+  geom_hline(yintercept = 0, linetype="dashed", col = "grey20") +
+  annotate("text", x = 59, y = 25, label = "Treatment",
+           col = "grey20", angle = 90) +
+  coord_cartesian(ylim = c(-20, 30)) +
+  xlab("Time") +
+  ylab("Gap (y - Synthetic Control)") +
+  theme_bw() + 
+  theme(legend.position=c(0.3,0.15), 
+        legend.box = "horizontal",
+        legend.background = element_rect(fill=NA))
+  
+
+df.t.test = data.frame(Beta = c(0, 0.5, 1),
+                       t = c(-4.7862, -5.8137, -8.2442),
+                       P = c(0.0001, 0.0001, 0.0001))
+
+fig.small = df.t.test %>% 
+  ggplot(aes(x = Beta, y = t)) +
+  annotate("rect", xmin = -0.2, xmax = 1.2,
+           ymin = -15, ymax = -2.63, alpha = .3) +
+  geom_line(size = 1) +
+  geom_point(size = 2, col = color.dsc) +
+  geom_hline(yintercept = 0, linetype="solid", col = "black") +
+  geom_hline(yintercept = -2.63, linetype="dashed", col = "grey20") +
+  annotate("text", x = 0.75, y = -4, 
+           label = "P < 0.01", col = "grey20") +
+  scale_x_continuous(breaks = c(0, 0.5, 1), 
+                     labels = c("0", "0.5", "1")) +
+  scale_y_continuous(breaks = c(-10, -5, 0)) +
+  coord_cartesian(ylim = c(-10, 0), xlim = c(0, 1)) +
+  xlab(expression(beta)) +
+  ylab("t") +
+  theme_bw() +
+  theme(axis.title.y = element_text(angle = 0, vjust = 0.5))
+
+
+fig.sim = fig.big + annotation_custom(ggplotGrob(fig.small),
+                                      xmin = 5, xmax = 45, 
+                                      ymin = 7, ymax = 30)
+
+ggsave("./figures/placebo_sim_0116.pdf",
+       fig.sim, width = 6, height = 4.5,
+       units = "in", limitsize = FALSE)
