e583f922d20c1dd80f6ad943765e36d74cfaa875
Jian Cao <jccit@caltech.edu>
2023-04-16 16:35:34 +0100

    Update figure_warping_path.R

--- R/figure_warping_path.R
+++ R/figure_warping_path.R
@@ -24,74 +24,90 @@ long = sin(seq(0, 2*pi, length.out = 100))
 x = c(short, long)
 y = c(long, short)
 
-res_dtw = dtw::dtw(x, y, keep = TRUE, step.pattern = dtw::symmetric1)
-
+# res_dtw = dtw::dtw(x, y, keep = TRUE, step.pattern = dtw::symmetric1)
 # n_line = 150
 # match_pairs = get_match_pairs(res_dtw, n_line)
 # match_pairs = match_pairs[c(seq(1,100, length.out = 20), 
 #                             seq(101,150, length.out = 20)),]
-# match_pairs = match_pairs[(1:n_line)%%10 %in% c(1,2),]
-match_pairs = rbind(
-  data.frame(ind_q = )
-)
-
-
+# match_pairs = data.frame(ind_q = c(1:100, rep(101:150, each = 2)),
+#                          ind_r = c(rep(1:50, each = 2), 51:150))
+# match_pairs = match_pairs[c(which((1:nrow(match_pairs))%%10 == 1), 200),]
 
 df_left = rbind(
   data.frame(time = c(1:50, 50:149),
              value = x + 5,
-             unit = "x",
-             group = c("black", rep("x_short", 48),
-                        "black", NA, rep("x_long", 98), "black")),
+             unit = "x"),
   data.frame(time = c(1:100, 100:149),
              value = y,
-             unit = "y",
-             group = c("black", rep("y_long", 98), 
-                        "black", NA, rep("y_short", 48), "black"))
+             unit = "y")
 )
 
-df_left$dtw = NA
-df_left$dtw[match_pairs$ind_r] = 1:nrow(match_pairs)
-df_left$dtw[match_pairs$ind_q + 150] = 1:nrow(match_pairs)
+points_x = c(seq(1,50, length.out = 5) %>% round() %>% .[1:4],
+           seq(50,150, length.out = 10) %>% round())
+
+points_y = c(seq(1,100, length.out = 10) %>% round() %>% .[1:9],
+             seq(100,150, length.out = 5) %>% round())
+
+df_match = rbind(
+  df_left[points_x[1:5],] %>% mutate(dtw = 1:5),
+  df_left[points_x[1:5],] %>% mutate(dtw = 6:10),
+  df_left[points_x[(1:5)*2+3],] %>% mutate(dtw = 11:15),
+  df_left[points_x[(1:5)*2+4],] %>% mutate(dtw = 16:20),
+  df_left[points_y[(1:5)*2-1]+150,] %>% mutate(dtw = 1:5),
+  df_left[points_y[(1:5)*2]+150,] %>% mutate(dtw = 6:10),
+  df_left[points_y[10:14]+150,] %>% mutate(dtw = 11:15),
+  df_left[points_y[10:14]+150,] %>% mutate(dtw = 16:20)
+)
+
+df_match = df_match %>% 
+  mutate(group = case_when(dtw >= 11 ~ "blue",
+                           dtw %in% c(3,8) ~ "black",
+                           dtw == 10 ~ "red_end",
+                           TRUE ~ "red"))
+
+# df_left$dtw = NA
+# df_left$dtw[match_pairs$ind_r] = 1:nrow(match_pairs)
+# df_left$dtw[match_pairs$ind_q + 150] = 1:nrow(match_pairs)
 
 
 size_line_ts = 1
 size_line_warp = 1
 size_point = 3
 color_main = "grey40"
-color_line_warp_0 = "grey40"
+color_line_warp_0 = "#fe4a49"
 color_line_warp_1 = "#fe4a49"
 color_line_warp_2 = "#2ab7ca"
 
 fig_left = df_left %>% 
   ggplot(aes(x = time, y = value, group = unit)) +
-  geom_line(data = df_left %>% filter(!is.na(dtw) &
-                                        group %in% c("x_short","y_long")),
+  geom_line(data = df_match %>% filter(group %in% c("red")),
             aes(group = dtw), color = color_line_warp_1,
-            linetype = "dashed", size = size_line_warp - 0.1) +
-  geom_line(data = df_left %>% filter(!is.na(dtw) & 
-                                        group %in% c("y_short","x_long")),
+            linetype = "dashed", linewidth = size_line_warp - 0.1) +
+  geom_line(data = df_match %>% filter(group == "red_end"),
+            aes(group = dtw), color = color_line_warp_0,
+            linetype = "twodash", linewidth = size_line_warp - 0.1) +
+  geom_line(data = df_match %>% filter(group == "black"),
+            aes(group = dtw), color = "black",
+            linetype = "dashed", linewidth = size_line_warp - 0.1) +
+  geom_line(data = df_match %>% filter(group %in% c("blue")),
             aes(group = dtw), color = color_line_warp_2,
-            linetype = "dashed", size = size_line_warp - 0.1) +
-  geom_line(data = df_left %>% filter(!is.na(dtw) & group == "black"),
-            aes(group = dtw), color = color_main,
-            linetype = "longdash", size = size_line_warp) +
+            linetype = "dashed", linewidth = size_line_warp - 0.1) +
   geom_line(size = size_line_ts, color = color_main) + 
-  geom_point(aes(x = time[1], y = value[1]),
-             color = color_main, size = size_point) +
-  geom_point(aes(x = time[50], y = value[50]),
-             color = color_main, size = size_point) +
-  geom_point(aes(x = time[150], y = value[150]),
-             color = color_main, size = size_point) +
-  geom_point(aes(x = time[151], y = value[151]),
-             color = color_main, size = size_point) +
-  geom_point(aes(x = time[250], y = value[250]),
-             color = color_main, size = size_point) +
-  geom_point(aes(x = time[300], y = value[300]),
-             color = color_main, size = size_point) +
-  annotate("text", x = 75, y = 6.5, label = "Y", fontface = "bold",
+  geom_point(aes(x = time[26], y = value[26]),
+             color = "black", size = size_point) +
+  geom_point(aes(x = time[45], y = value[195]),
+             color = "black", size = size_point) +
+  geom_point(aes(x = time[57], y = value[207]),
+             color = "black", size = size_point) +
+  annotate("text", x = df_left$time[26] + 2, y = df_left$value[26] + 0.5, label = "y[3]", fontface = "bold",
+           size = 6, col = "black", parse=TRUE) +
+  annotate("text", x = df_left$time[45], y = df_left$value[195] - 0.5, label = "x[5]", fontface = "bold",
+           size = 6, col = "black", parse=TRUE) +
+  annotate("text", x = df_left$time[57], y = df_left$value[207] - 0.5, label = "x[6]", fontface = "bold",
            size = 6, col = "black", parse=TRUE) +
   annotate("text", x = 75, y = -1.5, label = "X", fontface = "bold",
+           size = 6, col = "black", parse=TRUE) +
+  annotate("text", x = 75, y = 6.5, label = "Y", fontface = "bold",
            size = 6, col = "black", parse=TRUE) +
   ggtitle("DTW") +
   theme_minimal() +
@@ -106,11 +122,13 @@ fig_left = df_left %>%
         axis.ticks=element_blank())
 
 
-df_right = data.frame(ind_y = c(1:10, rep(11:15, each = 2)),
-                      ind_x = c(rep(1:5, each = 2), 6:15))
+df_right = data.frame(ind_y = c(1:10, rep(10:14, each = 2)),
+                      ind_x = c(rep(1:5, each = 2), 5:14))
 df_right$colors = c(rep("#2ab7ca", 10), rep("#fe4a49", 10))
 df_right$value = c(rep(1, 10), rep(1, 10))
-grid = expand.grid(1:15, 1:15)
+df_right = df_right[-11,]
+df_right$colors[10] = "#94818A"
+grid = expand.grid(1:14, 1:14)
 df_right = left_join(grid, df_right, by = c("Var1"="ind_x","Var2"="ind_y"))
 df_right$colors[is.na(df_right$colors)] = "white"
 
@@ -119,8 +137,20 @@ fig_right = df_right %>%
   geom_tile(aes(fill=colors),color = "grey40",
             lwd = 1.5,
             linetype = 1) +
-  scale_fill_manual(values = c("#fe4a49", "#2ab7ca", "white")) +
+  scale_fill_manual(values = c("#fe4a49", "#94818A", "#2ab7ca", "white")) +
   geom_text(aes(label = value), size = 5, color = "black") +
+  geom_segment(aes(x = 4.6, y = 3, xend = 0.5, yend = 3), linewidth = 1,
+               arrow = arrow(length = unit(0.2, "cm"))) +
+  geom_segment(aes(x = 5, y = 2.6, xend = 5, yend = 0.5), linewidth = 1,
+               arrow = arrow(length = unit(0.2, "cm"))) +
+  geom_segment(aes(x = 6, y = 2.6, xend = 6, yend = 0.5), linewidth = 1,
+               arrow = arrow(length = unit(0.2, "cm"))) +
+  annotate("text", x = -0.1, y = 3, label = "y[3]", fontface = "bold",
+           size = 6, col = "black", parse=TRUE) +
+  annotate("text", x = 5, y = -0.1, label = "x[5]", fontface = "bold",
+           size = 6, col = "black", parse=TRUE) +
+  annotate("text", x = 6, y = -0.1, label = "x[6]", fontface = "bold",
+           size = 6, col = "black", parse=TRUE) +
   xlab("X") +
   ylab("Y") +
   ggtitle("Warping Path") +
