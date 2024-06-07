874d55e14450d539b87de015dab9087e814e0a90
Jian Cao <jccit@caltech.edu>
2024-06-08 00:53:55 +0100

    update

--- rep/R/01_main_figures.R
+++ rep/R/01_main_figures.R
@@ -1,55 +1,55 @@
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_1.R'... ")
+cat("Runing script 'R/utility/Figure_1.R'...")
 source("./R/utility/Figure_1.R")
 cat("Done. Figure_1.pdf saved in 'figures/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_2.R'... ")
+cat("Runing script 'R/utility/Figure_2.R'...")
 source("./R/utility/Figure_2.R")
 cat("Done. Figure_2.pdf saved in 'figures/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_3.R'... ")
+cat("Runing script 'R/utility/Figure_3.R'...")
 source("./R/utility/Figure_3.R")
 cat("Done. Figure_3.pdf saved in 'figures/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_4.R'... ")
+cat("Runing script 'R/utility/Figure_4.R'...")
 source("./R/utility/Figure_4.R")
 cat("Done. Figure_4.pdf saved in 'figures/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_5_1.R'... ")
+cat("Runing script 'R/utility/Figure_5_1.R'...")
 source("./R/utility/Figure_5_1.R")
 cat("Done. Figure_5_1.Rds saved in 'data/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_5_2.R'... ")
+cat("Runing script 'R/utility/Figure_5_2.R'...")
 source("./R/utility/Figure_5_2.R")
 cat("Done. Figure_5_2.Rds saved in 'data/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_5_3.R'... ")
+cat("Runing script 'R/utility/Figure_5_3.R'...")
 source("./R/utility/Figure_5_3.R")
 cat("Done. Figure_5_3.Rds saved in 'data/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_5_all.R'... ")
+cat("Runing script 'R/utility/Figure_5_all.R'...")
 source("./R/utility/Figure_5_all.R")
 cat("Done. Figure_5.pdf saved in 'figures/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
--- rep/R/02_appendix_figures.R
+++ rep/R/02_appendix_figures.R
@@ -1,41 +1,41 @@
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_A1.R'... ")
+cat("Runing script 'R/utility/Figure_A1.R'...")
 source("./R/utility/Figure_A1.R")
 cat("Done. Figure_A1.pdf saved in 'figures/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_A2.R'... ")
+cat("Runing script 'R/utility/Figure_A2.R'...")
 source("./R/utility/Figure_A2.R")
 cat("Done. Figure_A2.pdf saved in 'figures/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_A3_1.R'... ")
+cat("Runing script 'R/utility/Figure_A3_1.R'...")
 source("./R/utility/Figure_A3_1.R")
 cat("Done. Figure_A3_1.Rds saved in 'data/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_A3_2.R'... ")
+cat("Runing script 'R/utility/Figure_A3_2.R'...")
 source("./R/utility/Figure_A3_2.R")
 cat("Done. Figure_A3_2.Rds saved in 'data/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_A3_3.R'... ")
+cat("Runing script 'R/utility/Figure_A3_3.R'...")
 source("./R/utility/Figure_A3_3.R")
 cat("Done. Figure_A3_3.Rds saved in 'data/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
 
 tic = Sys.time()
-cat("Runing script 'R/utility/Figure_A3_all.R'... ")
+cat("Runing script 'R/utility/Figure_A3_all.R'...")
 source("./R/utility/Figure_A3_all.R")
 cat("Done. Figure_A3.pdf saved in 'figures/'.\n")
 toc = Sys.time()
-toc - tic
+print(toc - tic)
